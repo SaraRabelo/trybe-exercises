fafd6ea5c965f711be96d2001dc680e2046b935c
SaraRabelo <rabelorsara@gmail.com>
2021-05-20 18:07:39 -0300

    Solução da questão 8a do Excercio do Bloco 4.1

---
+++ bloco_4/exercises_41_8a.js
@@ -0,0 +1,18 @@
+let num1 = 30;
+let num2 = 12;
+let num3 = 10
+;
+let resto1 = num1 %2;
+let resto2 = num2 % 2;
+let resto3 = num3 % 2;
+let resultado = false;
+
+
+//console.log(num1%2+"  " + num2%2+"  "+num3%2)
+
+if ((resto1 == 0) || (resto2 == 0) || (resto3 ==0)) {
+    resultado = true;
+        
+}
+
+console.log(resultado);
