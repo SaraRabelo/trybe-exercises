d88b9e3a6f24792fc1543f2036862985f0b2abf0
SaraRabelo <rabelorsara@gmail.com>
2021-05-21 10:50:02 -0300

    Solução do primeiro Exemplo do comando FOR

---
+++ bloco_4/exercises_41_Explo1.js
@@ -0,0 +1,5 @@
+let groceryList = ['Arroz', 'Feijão','Alface','Melancia'];
+console.log(groceryList);
+for (let index =0; index < groceryList.length;index +=1){
+    console.log(groceryList[index]);
+}
